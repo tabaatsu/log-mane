a04443b529eadeddb7d112903f773573c64b5749
makeAlot <mezase1050@gmail.com>
2023-10-22 22:43:42 +0900

    update docker-compose.yml to use PostgreSQL

--- logmane/settings.py
+++ logmane/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': 'postgrespassword',
         'HOST': 'db',
         'PORT': 5432,
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
