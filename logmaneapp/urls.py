a732af6559454d9a11dcf1db9436394581a12163
makeAlot <mezase1050@gmail.com>
2023-10-29 08:00:41 +0900

    add event record management function

--- logmaneapp/urls.py
+++ logmaneapp/urls.py
@@ -11,11 +11,13 @@ urlpatterns = [
     path('athlete/records/', views.athlete_records_form, name='athlete_records'),
     path('athlete/<int:pk>/delete/', views.AthleteDelete.as_view(), name='athlete_delete'),
     path('athlete/<int:pk>/update/', views.AthleteUpdate.as_view(), name='athlete_update'),
-    path('athlete/<int:pk>/records/delete/', views.AthleteRecordsDelete.as_view(), name='athlete_records_delete'),
-    path('athlete/<int:pk>/records/update/', views.AthleteRecordsUpdate.as_view(), name='athlete_records_update'),
+    path('athlete/records/<int:pk>/delete/', views.AthleteRecordsDelete.as_view(), name='athlete_records_delete'),
+    path('athlete/records/<int:pk>/update/', views.AthleteRecordsUpdate.as_view(), name='athlete_records_update'),
     path('event/create/', views.event_form, name='event_create'),
     path('event/<int:pk>/delete/', views.EventDelete.as_view(), name='event_delete'),
     path('event/<int:pk>/update/', views.EventUpdate.as_view(), name='event_update'),
     path('event/', views.EventListView.as_view(), name='event_list'),
-    
+    path('event/records/', views.event_records_form, name='event_records'),
+    path('event/records/<int:pk>/delete/', views.EventRecordsDelete.as_view(), name='event_records_delete'),
+    path('event/records/<int:pk>/update/', views.EventRecordsUpdate.as_view(), name='event_records_update'),
 ]
